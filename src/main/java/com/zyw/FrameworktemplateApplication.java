f08fe51beeca620d52b0708c0cf70f06d863500d
zyw <542665154@qq.com>
2020-04-09 10:35:57 +0800

    第一次提交

---
+++ src/main/java/com/zyw/FrameworktemplateApplication.java
@@ -0,0 +1,13 @@
+package com.zyw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworktemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworktemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zyw/FrameworktemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworktemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
